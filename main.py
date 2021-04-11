f1160436ec0b803e371d8a6d1dbc5f1936fb9122
E G <>
2021-04-11 16:35:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+number = int(input("Enter Number \n"))
+equation = (number*10)%10==0
+print("This is a multiple of")
+if ((number/10)*10)%10==0:
+  print("10.") 
+if ((number/9)*10)%10==0:
+  print("9.") 
+if ((number/8)*10)%10==0:
+  print("8.") 
+if ((number/7)*10)%10==0:
+  print("7.") 
+if ((number/6)*10)%10==0:
+  print("6.") 
+if ((number/5)*10)%10==0:
+  print("5.")
+if ((number/4)*10)%10==0:
+  print("4.")
+if ((number/3)*10)%10==0:
+  print("3.") 
+if ((number/2)*10)%10==0:
+  print("2.") 
+else:
+  print()
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
